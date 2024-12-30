c3ad9728303eedad15d725638ba153321bf35ea2
Sofía D. Alarcón <82108012+sofia-donoso21@users.noreply.github.com>
2024-12-30 18:13:48 -0300

    Se implementa sp parfa paginar

--- ClientesAPI/Controllers/ClientesController.cs
+++ ClientesAPI/Controllers/ClientesController.cs
@@ -23,7 +23,7 @@ namespace ClientesAPI.Controllers
             return await _context.Clientes.ToListAsync();
         }
 
-        // GET: api/Clientes/5
+        // GET: api/Clientes/id
         [HttpGet("{id}")]
         public async Task<ActionResult<Cliente>> GetCliente(int id)
         {
@@ -47,17 +47,35 @@ namespace ClientesAPI.Controllers
             return CreatedAtAction("GetCliente", new { id = cliente.ClienteID }, cliente);
         }
 
+
         // GET: api/Clientes/paginado
         [HttpGet("paginado")]
-        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientesPaginado(int page = 1, int pageSize = 10)
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientesPaginado( int page = 1, int pageSize = 10, string campo_orden = "OrdNombre", string orden = "Ascendente", string? nombre = null, string? pais = null)
         {
-            // Cálculo de la paginación
+            try
+            {
+                var parameters = new[]
+                {
+                    new Microsoft.Data.SqlClient.SqlParameter("@PageSize", pageSize),
+                    new Microsoft.Data.SqlClient.SqlParameter("@PageIndex", page),
+                    new Microsoft.Data.SqlClient.SqlParameter("@campo_orden", campo_orden),
+                    new Microsoft.Data.SqlClient.SqlParameter("@orden", orden),
+                    new Microsoft.Data.SqlClient.SqlParameter("@Nombre", nombre ?? (object)DBNull.Value),
+                    new Microsoft.Data.SqlClient.SqlParameter("@Pais", pais ?? (object)DBNull.Value)
+                };
+
             var clientes = await _context.Clientes
-                .Skip((page - 1) * pageSize)  // Saltar los primeros registros de acuerdo a la página solicitada
-                .Take(pageSize)  // Tomar solo la cantidad de registros indicada en pageSize
+                .FromSqlRaw("EXEC CLIENTES_PAGINACION_PAGINAS @PageSize, @PageIndex, @campo_orden, @orden, @Nombre, @Pais", parameters)
                 .ToListAsync();
 
             return clientes;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Error al ejecutar el procedimiento almacenado.", details = ex.Message });
+            }
         }
-    }
+
+
+     }
 }
